ea999d30451da6f585ed97f1e892a954474c5f7e
Huda-abn <abunada.huda@ada.ac.uk>
2026-04-27 22:48:47 +0000

    attempt_task2_BP1

--- stats_functions.py
+++ stats_functions.py
@@ -34,7 +34,35 @@ pass_length_count = {
     "20" : 0,
     "21" : 0
 }
-    
+
+pass_first_letter = {
+    "a" : 0,
+    "b" : 0,
+    "c" : 0,
+    "d" : 0,
+    "e" : 0,
+    "f" : 0,
+    "g" : 0,
+    "h" : 0,
+    "i" : 0,
+    "j" : 0,
+    "k" : 0,
+    "l" : 0,
+    "m" : 0,
+    "n" : 0,
+    "o" : 0,
+    "p" : 0,
+    "q" : 0,
+    "r" : 0,
+    "s" : 0,
+    "t" : 0,
+    "u" : 0,
+    "v" : 0,
+    "w" : 0,
+    "x" : 0,
+    "y" : 0,
+    "z" : 0,
+}   
 
 
 def process_and_print_lines(input_filename, delimiter):
@@ -47,15 +75,27 @@ def process_and_print_lines(input_filename, delimiter):
                 length = len(elements[1])
 
                 pass_length_count[str(length)] += 1
-            # print(pass_length_count)
+                #print(pass_length_count)
+                for element in elements[1]:
+                    first_letter = element[0]
+                    first_letter = first_letter.lower()
+                print (first_letter)
+                print(pass_first_letter)
 
-                    # if element_count == 20:
+                # pass_first_letter[first_letter] += 1
+        # with open("./letter_statistics.csv", "w", encoding = "utf-8") as file:
+        #     for i in range (len(pass_first_letter)):
+        #         file.write(f"letter {pass_first_letter[i]}, count : {pass_first_letter[str(i)]}\n")
+        #             # if element_count == 20:
         with open( OUTPUT_FILE,"w", encoding="utf-8") as file:
                 for i in range (len(pass_length_count)): 
                    file.write(f"chars: {i}, count: {pass_length_count[str(i)]}\n ")                    
 
     except FileNotFoundError:
         print(f"Error: The file {input_filename} was not found.")
+    
+#     Number of passwords beginning with A? B? C? (A through Z analysis)
+# Number of passwords starting with a number (0-9)
 
 
 delete_existing_output_file(OUTPUT_FILE)
